15e62b78abc89547863bd274a982f03e61bca676
Chidumaga <chidumaga@gmail.com>
2015-12-01 00:27:51 +0000

    detailed view of each category

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,12 +5,12 @@ angular.module('starter.controllers', [])
 	$scope.model.budget = 0;
 	$scope.progress = 0;
 	$scope.totalExpenditure = 0;
-
 	$scope.marginLeft = 0;
-	var totalWidth = 0;
-
 	$scope.selections = [];
+
 	var selectionCount = 0;
+	var totalWidth = 0;
+	var categoryExpenditure = [0,0]; //length of array equal to number of categories
 
 	// $scope.setBudgetValue = function(){
 	// 	console.log($scope.model.budget);
@@ -21,15 +21,15 @@ angular.module('starter.controllers', [])
 	}
 
 	$scope.addExpense = function(){
-		$scope.expense = {categories : [{"name":"Food", "color": "#e74c3c"},
-		{"name":"Groceries", "color": "#e67e22"}
+		$scope.expense = {categories : [{"key":0,"name":"Food", "color": "#e74c3c"},
+		{"key":1,"name":"Groceries", "color": "#e67e22"}
 		]};
 
 		// $scope.expense.category = $scope.expense.categories[0];
 	
 		$ionicPopup.show({
    			title: 'Add an expense',
-   			template: 'How much did you spend?<input type="number" placeholder="amount" ng-model="expense.amount"> <br> What did you spend it on? <br> <select ng-model="expense.category" ng-options="category as category.name for category in expense.categories" ng-change="updateLineColor(expense.category)"></select>',
+   			template: 'How much did you spend?<input type="number" placeholder="amount" ng-model="expense.amount"> <br> What did you spend it on? <br> <select ng-model="expense.category" ng-options="category as category.name for category in expense.categories" ng-change="updateCategory(expense.category)"></select>',
    			scope: $scope,
    			buttons: [
        			{ text: 'Cancel' },
@@ -41,24 +41,41 @@ angular.module('starter.controllers', [])
  			}).then(function(res) {
  				totalWidth += $scope.progress;
  				$scope.totalExpenditure += $scope.expense.amount;
- 				
+
  				calcExpenditure($scope.expense.amount, $scope.model.budget);
- 				setCssProperties();
+ 				setSelectionProperties();
  			});
 		}
 
-	$scope.updateLineColor= function(val){
-		$scope.lineColor = $scope.expense.category.color
+	$scope.updateCategory= function(val){
+		$scope.lineColor = val.color;
+		$scope.categoryName = val.name;
+		$scope.categoryKey = val.key;
+
+		categoryExpenditure[val.key] += $scope.expense.amount;
 	}
 
-	function setCssProperties(){
+	function setSelectionProperties(){
 		selectionCount += 1;
+		previousSelection = selectionCount-1
 
 		$scope.selections.push({});
-		$scope.selections[selectionCount-1].index = selectionCount;
-		$scope.selections[selectionCount-1].width = $scope.progress;
-		$scope.selections[selectionCount-1].marginLeft = totalWidth;
-		$scope.selections[selectionCount-1].lineColor = $scope.lineColor;
+		$scope.selections[previousSelection].index = selectionCount;
+		$scope.selections[previousSelection].width = $scope.progress;
+		$scope.selections[previousSelection].marginLeft = totalWidth;
+		$scope.selections[previousSelection].lineColor = $scope.lineColor;
+		$scope.selections[previousSelection].categoryName = $scope.categoryName;
+		$scope.selections[previousSelection].categoryKey = $scope.categoryKey;
+	}
+
+	$scope.getCategoryInfo = function(categoryName, categoryKey){
+		var totalSpent = categoryExpenditure[categoryKey];
+		var percentageSpent = (totalSpent/$scope.model.budget)*100
+
+		$ionicPopup.alert({
+     		title: '<b>'+categoryName+'</b>',
+     		template: 'You have spent:<br>'+' '+totalSpent+' '+'<br>which is<br>'+' '+percentageSpent+'%<br>'+'of your overall budget.'
+   		});
 	}
 
 })
